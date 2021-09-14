cb8ccf01671cb6e05a16c97fc145f411e8f4d126
Anthony Wang <ta180m@pm.me>
2021-09-14 17:00:30 +0000

    Continue working on section 3

---
+++ section1/1-print.py
@@ -0,0 +1,88 @@
+'''
+INTRODUCTION TO
+    ______  __________  ______  _   __
+   / __ \ \/ /_  __/ / / / __ \/ | / /
+  / /_/ /\  / / / / /_/ / / / /  |/ / 
+ / ____/ / / / / / __  / /_/ / /|  /  
+/_/     /_/ /_/ /_/ /_/\____/_/ |_/   
+                                      
+PRINTING OUTPUT
+
+Let's get started with learning Python!
+'''
+
+
+# This is a comment
+# It doesn't do anything
+
+# A multiple-line comment
+'''
+Your first podcast will be awful
+Your first video will be awful
+Your first article will be awful
+Your first art will be awful
+Your first photo will be awful
+Your first game will be awful
+But your first code will be perfect.
+Zero bugs and very clean code.
+It will be "Hello, world!"
+'''
+
+# Your first program
+# It prints Hello, world! in the console
+# The console is the black box on the right
+# Press "Run" at the top to see it in action!
+print("Hello, world!")
+
+# If you type python in the console, it will start up an interactive Python session
+# You know you are in a PYTHON console when it shows the >>>
+# Instead of the orange thing
+# You can then type in code and Python will run it as you go
+
+# Print some more text
+# To print text, it MUST BE IN QUOTES
+print("Python is fun!")
+print("I love Python!")
+print("My name is Billiam")
+
+
+'''
+Let's do Activity 1 now!
+Click on act1.py in the left sidebar to open Activity 1.
+'''
+
+
+# note that the 4 doesn't have quotes
+# it is an integer AKA a whole number
+print(4)
+
+# When we print a math expression, Python evaluates it
+# Note that this does not print text because there are no quotes!
+# Prints 4
+print(2 + 2)
+
+# Prints the LITERAL TEXT 2 + 2
+print("2 + 2")
+
+# Each print puts the result on a new line
+print("2 + 2 = ")
+print(2 + 2)
+
+# Python will evaluate complicated math expressions
+print(1729 - 42 - 13)
+print(9 + 10 * (7 * 5 - 3))
+
+# Prints a blank line
+print()
+
+# When you type something in the console, Python will automatically print it out
+# For example, if we type in the console:
+2 + 2
+# Prints 4 automatically, but ONLY in the console
+# If you click RUN, Python will not print stuff out unless you use print( )
+
+
+'''
+Now let's look at Activities 2 and 3!
+'''
+
--- section3/ch1.py
+++ section3/ch1.py
@@ -11,4 +11,3 @@ CHALLENGE 1: Todo (Lists)
 Simulate a todo list! Ask the user to input the action they want to do: append or remove. Then ask for what item to append or remove and print the list after every action!
 '''
 
-
