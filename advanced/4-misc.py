ab9857b6faab36c511c118492654e22cd7d309ea
Anthony Wang <ta180m@pm.me>
2021-09-16 17:53:22 +0000

    Convert section2/1-cond

--- advanced/4-misc.py
+++ advanced/4-misc.py
@@ -23,3 +23,27 @@ print(int("3A", 16))
 
 # file I/O
 
+
+
+# Some bonus data types (https://www.w3schools.com/python/python_datatypes.asp):
+complex1 = 1j
+list1 = ["apple", "banana", "cherry"]
+tuple1 = ("apple", "banana", "cherry")
+set1 = {"apple", "banana", "cherry"}
+range1 = range(6)
+dict1 = {"name" : "John", "age" : 36}
+bytes1 = b"Hello"
+bytearray1 = bytearray(5)
+
+
+print(type(complex1))
+print(type(list1))
+print(type(set1))
+print(type(range1))
+print(type(dict1))
+print(type(bytes1))
+print(type(bytearray1))
+
+print(type(type(123)))
+print(type(print))
+print(type(None))
